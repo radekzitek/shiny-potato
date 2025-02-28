52f9e52f8e9406af262ab46dc9c59a021c72d3e6
Radek Zítek <radekzitek@gmail.com>
2025-02-28 10:15:52 +0100

    Initialize Django project structure with ASGI, WSGI, settings, and user app

---
+++ eldis/eldis/urls.py
@@ -0,0 +1,17 @@
+"""
+URL Configuration for the eldis project.
+
+This module defines the URL routing for the entire project:
+- admin/: Django admin interface for site management
+- o/: OAuth2 provider endpoints for authentication
+
+For more information on Django URL configuration:
+https://docs.djangoproject.com/en/stable/topics/http/urls/
+"""
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
+]
